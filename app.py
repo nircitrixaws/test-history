65386b3c406d067dfa81ff39b5481f11e26ac4f6
nircitrixaws <80829171+nircitrixaws@users.noreply.github.com>
2022-04-27 22:32:15 +0530

    Update app.py
    updated with port 3306

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "Secret Key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/shipping'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost:3306/shipping'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
